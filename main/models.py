084372e78e43a420ef55e3a52bc9d32ae3d50842
riapis <fari.hafizh@ui.ac.id>
2023-09-26 23:52:09 +0700

    Tugas 4 done

--- main/models.py
+++ main/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     element = models.CharField(max_length=255)
-    amount = models.IntegerField()
-    power = models.IntegerField()
+    amount = models.PositiveIntegerField()
+    power = models.PositiveIntegerField()
     description = models.TextField()
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from main.views import show_main, add_card, show_xml, show_json, show_xml_by_id, show_json_by_id 
+from main.views import show_main, add_card, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user\
+, logout_user, increase_card, decrease_card, delete_card
 app_name = 'main'
 
 urlpatterns = [
@@ -9,4 +10,10 @@ urlpatterns = [
     path('json/', show_json, name='show_json'), 
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('product/<int:product_id>/increase/', increase_card, name='increase_card'),
+    path('product/<int:product_id>/decrease/', decrease_card, name='decrease_card'),
+     path('delete_card/<int:product_id>/', delete_card, name='delete_card'),
 ]
